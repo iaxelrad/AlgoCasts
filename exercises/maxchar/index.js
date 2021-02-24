01cf48bd8e33e495895d1da5cb984b5efdc6c1d5
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-24 22:57:51 +0200

    MaxChar in a string solution.

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]; //The value
+      maxChar = char; //The key
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
