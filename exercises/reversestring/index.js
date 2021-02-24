3a78a296f67b6cb9b463dfe36a4de24984e1d4a4
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-24 08:53:57 +0200

    All solutions for reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -10,4 +10,21 @@ function reverse(str) {
   return str.split('').reduce((rev, char) => char + rev, '');
 }
 
+//Solution 1
+
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// Solution 2
+
+// function reverse(str) {
+//   let reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+
+//   return reversed;
+// }
+
 module.exports = reverse;
