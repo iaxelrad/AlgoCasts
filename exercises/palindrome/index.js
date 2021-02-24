c941f60e05960023e2023052eaa58cb8a4091077
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-24 08:56:56 +0200

    palindrome solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,9 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  const reversedStr = str.split('').reverse().join('');
+  return str === reversedStr;
+}
 
 module.exports = palindrome;
