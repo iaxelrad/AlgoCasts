9641a5dd8ea64b07ae0b30b126f41aac403d7d07
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-26 10:01:25 +0200

    Capitalization Sol.1 - string.slice() method.

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,16 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let words = [];
+  // const strings = str.split(' ');
+  // strings.forEach(word => {
+  //   words.push(word[0].toUpperCase() + word.slice(1));
+  // });
+  for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(' ');
+}
 
 module.exports = capitalize;
