87e89828f7f11072183f5f9bf68b8e5fe1f0b3d7
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-27 21:11:30 +0200

    linked list removeAt method solution.

--- exercises/data-structures/linkedlist/index.js
+++ exercises/data-structures/linkedlist/index.js
@@ -79,6 +79,40 @@ class LinkedList {
       this.head = new Node(data);
     }
   };
+
+  getAt = index => {
+    let counter = 0;
+    let node = this.head;
+    if (index > this.size) {
+      return;
+    }
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  };
+
+  removeAt = index => {
+    if (!this.head) {
+      //Empty list edge case
+      return;
+    }
+    if (index === 0) {
+      // Removing first node case.
+      return (this.head = this.head.next);
+    }
+
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      //Index out of bounds edge case.
+      return;
+    }
+    previous.next = previous.next.next;
+  };
 }
 
 module.exports = { Node, LinkedList };
