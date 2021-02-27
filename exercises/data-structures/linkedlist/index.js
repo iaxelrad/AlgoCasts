177f59620294cda82aaa5bdd878f3d0964afe7cf
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-27 17:41:52 +0200

    LinkedList - Implemented clear, removeFirst and removeLast

--- exercises/data-structures/linkedlist/index.js
+++ exercises/data-structures/linkedlist/index.js
@@ -42,6 +42,33 @@ class LinkedList {
     }
     return node;
   };
+
+  clear = () => {
+    this.head = null;
+  };
+
+  removeFirst = () => {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+  };
+
+  removeLast = () => {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      return (this.head = null);
+    }
+    let node = this.head.next;
+    let previous = this.head;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  };
 }
 
 module.exports = { Node, LinkedList };
